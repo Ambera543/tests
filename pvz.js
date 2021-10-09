943579a78996a7fe8668b3b5bd78fbcace231681
Ambera543 <86227272+Ambera543@users.noreply.github.com>
2021-10-09 15:47:32 +0300

    astuntas

---
+++ pvz.js
@@ -0,0 +1,13 @@
+// let randomNumber = 0;
+// let randomNumberCount = 0;
+//  while (randomNumber=5 ){
+//  randomNumber= Math.floor((Math.random() * 10)+1);
+//  randomNumberCount++;}
+//  console.log(randomNumberCount, randomNumber);
+// console.log("8---------");
+ sum = 100;
+var x = 0;
+while(x !== sum){
+  x = Math.floor((Math.random() * 10) + 1);
+  console.log(x);
+}
