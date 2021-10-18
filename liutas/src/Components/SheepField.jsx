1580c20e30c800a53ab270a043c69bb7ea975a25
Ambera543 <g.eismontiene@gmai.com>
2021-10-18 16:54:20 +0300

    nemu darbai 18d

--- liutas/src/Components/SheepField.jsx
+++ liutas/src/Components/SheepField.jsx
@@ -1,46 +1,46 @@
-import { useState } from "react";
 
 
-function SheepField() {
 
-    const [counter, setCounter] = useState({count: 0});
-    const [bgColor, setBgColor] = useState('darkturquoise')
+// function SheepField() {
 
-    const clickSheep = what => {
-        console.log(what);
-        let newCounter = counter.count;
-        switch(what) {
-            case 'one': 
-                newCounter++;
-                break;
-            case 'ten': 
-                newCounter+=10;
-                break;
-            default: 
-                newCounter=0;
-                setBgColor('white');
-        }
-        setCounter({count:newCounter});
-    }
+//     const [counter, setCounter] = useState({count: 0});
+//     const [bgColor, setBgColor] = useState('darkturquoise')
 
-    const changeColor = () => {
-        if (bgColor === 'darkturquoise') {
-            setBgColor('red');
-        }
-        else {
-            setBgColor('darkturquoise');
-        }
-    }
+//     const clickSheep = what => {
+//         console.log(what);
+//         let newCounter = counter.count;
+//         switch(what) {
+//             case 'one': 
+//                 newCounter++;
+//                 break;import { useState } from "react";
+//             case 'ten': 
+//                 newCounter+=10;
+//                 break;
+//             default: 
+//                 newCounter=0;
+//                 setBgColor('white');
+//         }
+//         setCounter({count:newCounter});
+//     }
 
-    return (
-        <div className="cow-field">
-            <div className="counter" style={{color:bgColor}}>{counter.count}</div>
-            <button onClick={()=>clickSheep('one')}>Add Sheep</button>
-            <button onClick={()=>clickSheep('ten')}>Add 10 Sheeps</button>
-            <button onClick={()=>clickSheep('go')}>Go Home</button>
-            <button onClick={changeColor}>Change Color</button>
-        </div>
-    )
-}
+//     const changeColor = () => {
+//         if (bgColor === 'darkturquoise') {
+//             setBgColor('red');
+//         }
+//         else {
+//             setBgColor('darkturquoise');
+//         }
+//     }
 
-export default SheepField;
+//     return (
+//         <div className="cow-field">
+//             <div className="counter" style={{color:bgColor}}>{counter.count}</div>
+//             <button onClick={()=>clickSheep('one')}>Add Sheep</button>
+//             <button onClick={()=>clickSheep('ten')}>Add 10 Sheeps</button>
+//             <button onClick={()=>clickSheep('go')}>Go Home</button>
+//             <button onClick={changeColor}>Change Color</button>
+//         </div>
+//     )
+// }
+
+// export default SheepField;
