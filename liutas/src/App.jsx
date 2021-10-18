2546bf4fbc28b85af67e421cd1ebdbfedf96e5ba
Ambera543 <g.eismontiene@gmai.com>
2021-10-18 16:54:31 +0300

    commit

--- liutas/src/App.jsx
+++ liutas/src/App.jsx
@@ -1,17 +1,30 @@
-import CowField from './Components/CowField';
-import SheepField from './Components/SheepField';
+import  TwoCircles from './Components/TwoCircles'
 
 function App() {
 
     return (
         <>
-            <h3>HeLLo, Cows!</h3>
-            <CowField counterColor={'darkturquoise'}/>
-            <h3>HeLLo, Sheeps!</h3>
-            <SheepField/>
+           
+            <TwoCircles/>
+        
         </>
     )
 }
 
 
 export default App;
+
+// import CowField from './Components/CowField';
+// import SheepField from './Components/SheepField';
+
+// function App() {
+
+//     return (
+//         <>
+//             <h3>HeLLo, Cows!</h3>
+//             <CowField counterColor={'darkturquoise'}/>
+//             <h3>HeLLo, Sheeps!</h3>
+//             <SheepField/>
+//         </>
+//     )
+// }
--- liutas/src/Components/CowField.jsx
+++ liutas/src/Components/CowField.jsx
@@ -1,52 +1,52 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 
-function CowField({counterColor}) {
+// function CowField({counterColor}) {
 
-    const [counter, setCounter] = useState(0);
+//     const [counter, setCounter] = useState(0);
 
-    const addCow = () => {
+//     const addCow = () => {
 
-        // console.log('Cow will be added soon');
+//         // console.log('Cow will be added soon');
 
-        //BLOGAI 
-        // counter++;
+//         //BLOGAI 
+//         // counter++;
 
-        const newCounter = counter + 1;
+//         const newCounter = counter + 1;
 
-        setCounter(newCounter);
+//         setCounter(newCounter);
 
-        // console.log(counter);
+//         // console.log(counter);
 
         
-        // const masyvas = ['Labas', 'Karve'];
-        // console.log(masyvas);
+//         // const masyvas = ['Labas', 'Karve'];
+//         // console.log(masyvas);
 
-        // const pasisveikinimas = masyvas[0];
-        // const vardas = masyvas[1];
-        // ====> const [pasisveikinimas, vardas] = masyvas;
+//         // const pasisveikinimas = masyvas[0];
+//         // const vardas = masyvas[1];
+//         // ====> const [pasisveikinimas, vardas] = masyvas;
 
-        // console.log(pasisveikinimas, vardas);
+//         // console.log(pasisveikinimas, vardas);
 
-    }
+//     }
 
-    const add10Cows = () => {
-        const newCounter = counter + 10;
-        setCounter(newCounter);
-    }
+//     const add10Cows = () => {
+//         const newCounter = counter + 10;
+//         setCounter(newCounter);
+//     }
 
-    const goHome = () => {
-        setCounter(0);
-    }
+//     const goHome = () => {
+//         setCounter(0);
+//     }
 
-    return (
-        <div className="cow-field">
-            <div className="counter" style={{color:counterColor}}>{counter}</div>
-            <button onClick={addCow}>Add Cow</button>
-            <button onClick={add10Cows}>Add 10 Cows</button>
-            <button onClick={goHome}>Go Home</button>
-        </div>
-    )
-}
+//     return (
+//         <div className="cow-field">
+//             <div className="counter" style={{color:counterColor}}>{counter}</div>
+//             <button onClick={addCow}>Add Cow</button>
+//             <button onClick={add10Cows}>Add 10 Cows</button>
+//             <button onClick={goHome}>Go Home</button>
+//         </div>
+//     )
+// }
 
-export default CowField;
+// export default CowField;
